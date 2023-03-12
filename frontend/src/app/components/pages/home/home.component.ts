11b2ed9158f6d126267ddb7418ef3de65c08cc88
Muhammad Yeasin <yea.dev.de@gmail.com>
2023-03-12 20:52:51 +0100

    tag service

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -17,6 +17,8 @@ export class HomeComponent implements OnInit {
     activatedRoute.params.subscribe((params) => {
       if (params.searchTerm)
         this.foods = this.foodService.getAllFoodBySearchTerm(params.searchTerm);
+      else if (params.tag)
+        this.foods = this.foodService.getAllFoodByTag(params.tag);
       else this.foods = foodService.getAll();
     });
   }
