fab50dca59222b9cc12ed75a3bd27c5d2803bba3
Muhammad Yeasin <yea.dev.de@gmail.com>
2023-03-12 18:39:12 +0100

    search component with functionality

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { RatingModule } from 'ng-starrating';
+import { SearchComponent } from './components/partials/search/search.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent],
   imports: [BrowserModule, AppRoutingModule, RatingModule],
   providers: [],
   bootstrap: [AppComponent],
