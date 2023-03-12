a7f276aa607e376b6ded0e0c5e427844ef6eb05b
Muhammad Yeasin <yea.dev.de@gmail.com>
2023-03-12 18:08:35 +0100

    search service

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -9,8 +10,15 @@ import { Food } from 'src/app/shared/models/Food';
 })
 export class HomeComponent implements OnInit {
   foods: Food[] = [];
-  constructor(private foodService: FoodService) {
-    this.foods = foodService.getAll();
+  constructor(
+    private foodService: FoodService,
+    activatedRoute: ActivatedRoute
+  ) {
+    activatedRoute.params.subscribe((params) => {
+      if (params.searchTerm)
+        this.foods = this.foodService.getAllFoodBySearchTerm(params.searchTerm);
+      else this.foods = foodService.getAll();
+    });
   }
 
   ngOnInit(): void {}
