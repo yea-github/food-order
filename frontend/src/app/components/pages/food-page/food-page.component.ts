b31e4f44cffb8782a79008473d56b3d51c620b3a
Muhammad Yeasin <yea.dev.de@gmail.com>
2023-03-14 01:48:28 +0100

    cart page component, title component

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -11,11 +12,21 @@ import { Food } from 'src/app/shared/models/Food';
 export class FoodPageComponent implements OnInit {
   food!: Food;
 
-  constructor(activatedRoute: ActivatedRoute, foodService: FoodService) {
+  constructor(
+    activatedRoute: ActivatedRoute,
+    foodService: FoodService,
+    private cartService: CartService,
+    private router: Router
+  ) {
     activatedRoute.params.subscribe((params) => {
       if (params.id) this.food = foodService.getFoodById(params.id);
     });
   }
 
   ngOnInit(): void {}
+
+  addToCart() {
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
+  }
 }
