0e07e1c895a67c1d82590846a787657813b5d6f1
Muhammad Yeasin <yea.dev.de@gmail.com>
2023-03-15 13:18:08 +0100

    filter API

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { sample_foods } from "./data";
 
 const app = express();
 
@@ -12,6 +13,20 @@ app.use(
 
 app.get("/api/hello", (req, res) => {
   res.send("Hello Food Order");
+});
+
+app.get("/api/foods", (req, res) => {
+  res.send(sample_foods);
+});
+
+app.get("/api/foods/search/:searchTerm", (req, res) => {
+  const searchTerm = req.params.searchTerm;
+
+  const foods = sample_foods.filter((food) =>
+    food.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  res.send(foods);
 });
 
 const port = 5000;
