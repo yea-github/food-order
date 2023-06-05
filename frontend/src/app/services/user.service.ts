503899316f710bcff25b7b7468d7f07498af5c4f
Muhammad Yeasin <yea.dev.de@gmail.com>
2023-06-06 00:07:40 +0200

    user info manage by localstorage

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -6,11 +6,15 @@ import { HttpClient } from '@angular/common/http';
 import { USER_LOGIN_URL } from '../shared/constants/urls';
 import { ToastrService } from 'ngx-toastr';
 
+const USER_KEY = 'User';
+
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private userSubject = new BehaviorSubject<User>(new User());
+  private userSubject = new BehaviorSubject<User>(
+    this.getUserFromLocalStorage()
+  );
   public userObservable: Observable<User>;
   constructor(private http: HttpClient, private toastrService: ToastrService) {
     this.userObservable = this.userSubject.asObservable();
@@ -20,6 +24,7 @@ export class UserService {
     return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
       tap({
         next: (user) => {
+          this.setUserToLocalStorage(user);
           this.userSubject.next(user);
           this.toastrService.success(
             'Wellcome to Food Order ' + user.name,
@@ -31,5 +36,15 @@ export class UserService {
         },
       })
     );
+  }
+
+  private setUserToLocalStorage(user: User) {
+    localStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  private getUserFromLocalStorage(): User {
+    const userJson = localStorage.getItem(USER_KEY);
+    if (userJson) return JSON.parse(userJson) as User;
+    return new User();
   }
 }
